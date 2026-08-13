492cbd2799b9f043cb2c621a2573b69b708688dc
clclement2 <clement.verin@etu.uca.fr>
2026-08-14 00:51:22 +0200

    Script d'apparition fonctionnel !

--- Portfolio.js
+++ Portfolio.js
@@ -1,18 +1,35 @@
+class HeaderTemplate extends HTMLElement
+{
+    constructor()
+    {
+        super(); // Il faut forcément dériver de HTML Element
+
+        this.header = document.querySelector('.header-main')
+
+        this.classList.add('fade-In-Loading')
+    }
+}
 
 class InfoCardTemplate extends HTMLElement
 {
-    constructor(Infocards, oberserver)
+    constructor()
     {
-        super();
+        super(); // Il faut forcément dériver de HTML Element
 
-        this.infoCards = document.querySelectorAll('.info-card-template');
+        this.infoCards = document.querySelectorAll('.info-card-template'); // Sélectionne toutes les balises avec la classe de style correspondante
+        console.log(this.infoCards.length)
 
-        this.observer = new IntersectionObserver(
+        this.setupObserver();
+    }
+
+    setupObserver()
+    {
+        const observer = new IntersectionObserver(
             (entries, observer) => {
                 entries.forEach(entry => {
                     this.AppearingAnimation(entry, observer); 
                 })
-            }, {threshold : 0.5}
+            }, {threshold : 0.5} // Pourcentage d'exposition pour apparition
         );
 
         this.infoCards.forEach(card => {observer.observe(card);});
@@ -28,4 +45,5 @@ class InfoCardTemplate extends HTMLElement
     }
 }
 
-customElements.define('info-card', InfoCardTemplate);
+customElements.define('header-main', HeaderTemplate);
+customElements.define('info-card', InfoCardTemplate); // Définit la balise correspondante comme customisée
