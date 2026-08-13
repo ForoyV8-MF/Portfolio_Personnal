b7ae2f1a66d06731ba70fe5c3f714674bdb99e53
clclement2 <clement.verin@etu.uca.fr>
2026-08-13 20:19:50 +0200

    Premiers Essais d'un script + redimensionnements

---
+++ Portfolio.js
@@ -0,0 +1,31 @@
+
+class InfoCardTemplate extends HTMLElement
+{
+    constructor(Infocards, oberserver)
+    {
+        super();
+
+        this.infoCards = document.querySelectorAll('.info-card-template');
+
+        this.observer = new IntersectionObserver(
+            (entries, observer) => {
+                entries.forEach(entry => {
+                    this.AppearingAnimation(entry, observer); 
+                })
+            }, {threshold : 0.5}
+        );
+
+        this.infoCards.forEach(card => {observer.observe(card);});
+    }
+
+    AppearingAnimation(entry, observer)
+    {
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add('fade-In-Loading');
+            observer.unobserve(entry.target);
+        }
+    }
+}
+
+customElements.define('info-card-template', InfoCardTemplate);
